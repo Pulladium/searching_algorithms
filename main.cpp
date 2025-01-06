8a8bc7d5873623135e27f9500024fccc2f991abc
Artem Vozhov <vozhov.artem1@gmail.com>
2025-01-06 18:54:56 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+int main() {
+    std::string text = "ABAAABCDABAAABCD";
+    std::string pattern = "ABC";
+
+    std::cout << "Pattern found at positions: ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
